fe5d98a8be1789195715525af6ffb756ee1038c9
CMK <cirno.mainasuk@gmail.com>
2019-09-02 15:23:52 +0800

    feat: add Splash package

--- Package.swift
+++ Package.swift
@@ -8,9 +8,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor-community/leaf-markdown.git", from: "2.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/JohnSundell/Splash.git", from: "0.8.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor", "LeafMarkdown", "Yams"]),
+        .target(name: "App", dependencies: ["Leaf", "Vapor", "LeafMarkdown", "Yams", "Splash"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
