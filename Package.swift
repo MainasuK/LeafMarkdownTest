3d8d29e3542d312e7d21da011d07f0224856838f
CMK <cirno.mainasuk@gmail.com>
2019-09-02 15:22:15 +0800

    feat: add Yams package

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor-community/leaf-markdown.git", from: "2.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor", "LeafMarkdown"]),
+        .target(name: "App", dependencies: ["Leaf", "Vapor", "LeafMarkdown", "Yams"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
